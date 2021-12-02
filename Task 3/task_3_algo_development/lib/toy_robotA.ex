553208b806cc05eda7271a526a985087e45d2118
Toshan Luktuke <toshan1603@gmail.com>
2021-12-02 09:48:53 +0530

    Added Task 3

---
+++ Task 3/task_3_algo_development/lib/toy_robotA.ex
@@ -0,0 +1,165 @@
+defmodule CLI.ToyRobotA do
+  # max x-coordinate of table top
+  @table_top_x 5
+  # max y-coordinate of table top
+  @table_top_y :e
+  # mapping of y-coordinates
+  @robot_map_y_atom_to_num %{:a => 1, :b => 2, :c => 3, :d => 4, :e => 5}
+
+  @doc """
+  Places the robot to the default position of (1, A, North)
+
+  Examples:
+
+      iex> CLI.ToyRobotA.place
+      {:ok, %CLI.Position{facing: :north, x: 1, y: :a}}
+  """
+  def place do
+    {:ok, %CLI.Position{}}
+  end
+
+  def place(x, y, _facing) when x < 1 or y < :a or x > @table_top_x or y > @table_top_y do
+    {:failure, "Invalid position"}
+  end
+
+  def place(_x, _y, facing)
+  when facing not in [:north, :east, :south, :west]
+  do
+    {:failure, "Invalid facing direction"}
+  end
+
+  @doc """
+  Places the robot to the provided position of (x, y, facing),
+  but prevents it to be placed outside of the table and facing invalid direction.
+
+  Examples:
+
+      iex> CLI.ToyRobotA.place(1, :b, :south)
+      {:ok, %CLI.Position{facing: :south, x: 1, y: :b}}
+
+      iex> CLI.ToyRobotA.place(-1, :f, :north)
+      {:failure, "Invalid position"}
+
+      iex> CLI.ToyRobotA.place(3, :c, :north_east)
+      {:failure, "Invalid facing direction"}
+  """
+  def place(x, y, facing) do
+    # IO.puts String.upcase("A I'm placed at => #{x},#{y},#{facing}")
+    {:ok, %CLI.Position{x: x, y: y, facing: facing}}
+  end
+
+  @doc """
+  Provide START position to the robot as given location of (x, y, facing) and place it.
+  """
+  def start(x, y, facing) do
+    ###########################
+    ## complete this funcion ##
+    ###########################
+  end
+
+  def stop(_robot, goal_x, goal_y, _cli_proc_name) when goal_x < 1 or goal_y < :a or goal_x > @table_top_x or goal_y > @table_top_y do
+    {:failure, "Invalid STOP position"}
+  end
+
+  @doc """
+  Provide GOAL positions to the robot as given location of [(x1, y1),(x2, y2),..] and plan the path from START to these locations.
+  Passing the CLI Server process name that will be used to send robot's current status after each action is taken.
+  Spawn a process and register it with name ':client_toyrobotA' which is used by CLI Server to send an
+  indication for the presence of obstacle ahead of robot's current position and facing.
+  """
+  def stop(robot, goal_locs, cli_proc_name) do
+    ###########################
+    ## complete this funcion ##
+    ###########################
+  end
+
+  @doc """
+  Send Toy Robot's current status i.e. location (x, y) and facing
+  to the CLI Server process after each action is taken.
+  Listen to the CLI Server and wait for the message indicating the presence of obstacle.
+  The message with the format: '{:obstacle_presence, < true or false >}'.
+  """
+  def send_robot_status(%CLI.Position{x: x, y: y, facing: facing} = _robot, cli_proc_name) do
+    send(cli_proc_name, {:toyrobotA_status, x, y, facing})
+    # IO.puts("Sent by Toy Robot Client: #{x}, #{y}, #{facing}")
+    listen_from_server()
+  end
+
+  @doc """
+  Listen to the CLI Server and wait for the message indicating the presence of obstacle.
+  The message with the format: '{:obstacle_presence, < true or false >}'.
+  """
+  def listen_from_server() do
+    receive do
+      {:obstacle_presence, is_obs_ahead} ->
+        is_obs_ahead
+    end
+  end
+
+  @doc """
+  Provides the report of the robot's current position
+
+  Examples:
+
+      iex> {:ok, robot} = CLI.ToyRobotA.place(2, :b, :west)
+      iex> CLI.ToyRobotA.report(robot)
+      {2, :b, :west}
+  """
+  def report(%CLI.Position{x: x, y: y, facing: facing} = _robot) do
+    {x, y, facing}
+  end
+
+  @directions_to_the_right %{north: :east, east: :south, south: :west, west: :north}
+  @doc """
+  Rotates the robot to the right
+  """
+  def right(%CLI.Position{facing: facing} = robot) do
+    %CLI.Position{robot | facing: @directions_to_the_right[facing]}
+  end
+
+  @directions_to_the_left Enum.map(@directions_to_the_right, fn {from, to} -> {to, from} end)
+  @doc """
+  Rotates the robot to the left
+  """
+  def left(%CLI.Position{facing: facing} = robot) do
+    %CLI.Position{robot | facing: @directions_to_the_left[facing]}
+  end
+
+  @doc """
+  Moves the robot to the north, but prevents it to fall
+  """
+  def move(%CLI.Position{x: _, y: y, facing: :north} = robot) when y < @table_top_y do
+    %CLI.Position{robot | y: Enum.find(@robot_map_y_atom_to_num, fn {_, val} -> val == Map.get(@robot_map_y_atom_to_num, y) + 1 end) |> elem(0)}
+  end
+
+  @doc """
+  Moves the robot to the east, but prevents it to fall
+  """
+  def move(%CLI.Position{x: x, y: _, facing: :east} = robot) when x < @table_top_x do
+    %CLI.Position{robot | x: x + 1}
+  end
+
+  @doc """
+  Moves the robot to the south, but prevents it to fall
+  """
+  def move(%CLI.Position{x: _, y: y, facing: :south} = robot) when y > :a do
+    %CLI.Position{robot | y: Enum.find(@robot_map_y_atom_to_num, fn {_, val} -> val == Map.get(@robot_map_y_atom_to_num, y) - 1 end) |> elem(0)}
+  end
+
+  @doc """
+  Moves the robot to the west, but prevents it to fall
+  """
+  def move(%CLI.Position{x: x, y: _, facing: :west} = robot) when x > 1 do
+    %CLI.Position{robot | x: x - 1}
+  end
+
+  @doc """
+  Does not change the position of the robot.
+  This function used as fallback if the robot cannot move outside the table
+  """
+  def move(robot), do: robot
+
+  def failure do
+    raise "Connection has been lost"
+  end
+end
