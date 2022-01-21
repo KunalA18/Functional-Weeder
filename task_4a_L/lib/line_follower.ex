27e03060e595411c1d8f577ece8a5e755688d64b
KunalA18 <kunalagarwal1072002@gmail.com>
2022-01-21 14:21:06 +0530

    did some pid tuning

--- task_4a_L/lib/line_follower.ex
+++ task_4a_L/lib/line_follower.ex
@@ -1,5 +1,4 @@
 defmodule Line_follower do
-
   require Logger
   use Bitwise
   alias Circuits.GPIO
@@ -13,7 +12,14 @@ defmodule Line_follower do
 
   @ref_atoms [:cs, :clock, :address, :dataout]
   @lf_sensor_data %{sensor0: 0, sensor1: 0, sensor2: 0, sensor3: 0, sensor4: 0, sensor5: 0}
-  @lf_sensor_map %{0 => :sensor0, 1 => :sensor1, 2 => :sensor2, 3 => :sensor3, 4 => :sensor4, 5 => :sensor5}
+  @lf_sensor_map %{
+    0 => :sensor0,
+    1 => :sensor1,
+    2 => :sensor2,
+    3 => :sensor3,
+    4 => :sensor4,
+    5 => :sensor5
+  }
 
   @forward [0, 1, 1, 0]
   @backward [0, 1, 0, 1]
@@ -33,46 +39,56 @@ defmodule Line_follower do
   @higher_duty_cycle 140
   # float left_duty_cycle = 0, right_duty_cycle = 0;
 
-
   def initialize do
-    error=0
-    prev_error=0
+    error = 0
+    prev_error = 0
     cumulative_error = 0
     left_duty_cycle = 0
     right_duty_cycle = 0
-    line_follow(error,prev_error,cumulative_error,left_duty_cycle,right_duty_cycle)
+    line_follow(error, prev_error, cumulative_error, left_duty_cycle, right_duty_cycle)
   end
 
-  def line_follow(error,prev_error,cumulative_error,left_duty_cycle,right_duty_cycle) do
+  def line_follow(error, prev_error, cumulative_error, left_duty_cycle, right_duty_cycle) do
     map_sens_list = test_wlf_sensors()
     IO.inspect(map_sens_list)
     # error = calculate_error(map_sens_list)
-    {error,prev_error} = calculate_error(map_sens_list,error,prev_error)
-    {error,prev_error,correction} = calculate_correction(error,prev_error,cumulative_error)
+    {error, prev_error} = calculate_error(map_sens_list, error, prev_error)
+    {error, prev_error, correction} = calculate_correction(error, prev_error, cumulative_error)
 
     left_duty_cycle = @optimum_duty_cycle - correction
     right_duty_cycle = @optimum_duty_cycle + correction
 
-    left_duty_cycle = if left_duty_cycle < @lower_duty_cycle do
-                        left_duty_cycle = @lower_duty_cycle
-                      end
-    left_duty_cycle = if left_duty_cycle > @higher_duty_cycle do
-                        left_duty_cycle = @higher_duty_cycle
-                      end
-    right_duty_cycle = if right_duty_cycle < @lower_duty_cycle do
-                        right_duty_cycle = @lower_duty_cycle
-                      end
-    right_duty_cycle = if right_duty_cycle > @higher_duty_cycle do
-                        right_duty_cycle = @higher_duty_cycle
-                      end
+    left_duty_cycle =
+      if left_duty_cycle < @lower_duty_cycle do
+        left_duty_cycle = @lower_duty_cycle
+      end
+
+    left_duty_cycle =
+      if left_duty_cycle > @higher_duty_cycle do
+        left_duty_cycle = @higher_duty_cycle
+      end
+
+    right_duty_cycle =
+      if right_duty_cycle < @lower_duty_cycle do
+        right_duty_cycle = @lower_duty_cycle
+      end
+
+    right_duty_cycle =
+      if right_duty_cycle > @higher_duty_cycle do
+        right_duty_cycle = @higher_duty_cycle
+      end
+
     motor_ref = Enum.map(@motor_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
     motor_action(motor_ref, @forward)
-    my_motion(left_duty_cycle,right_duty_cycle)
+    my_motion(left_duty_cycle, right_duty_cycle)
+    # IO.inspect(correction)
+    # IO.inspect(left_duty_cycle)
+    # IO.inspect(right_duty_cycle)
 
-    line_follow(error,prev_error,cumulative_error,left_duty_cycle,right_duty_cycle)
+    line_follow(error, prev_error, cumulative_error, left_duty_cycle, right_duty_cycle)
   end
 
-  def calculate_error(map_sens_list,error,prev_error) do
+  def calculate_error(map_sens_list, error, prev_error) do
     all_black_flag = 1
     weighted_sum = 0
     sum = 0
@@ -88,80 +104,99 @@ defmodule Line_follower do
     #                                                end
     #                                                all_black_flag
     #                                               end)
-    all_black_flag = if Enum.at(map_sens_list,0) > @black_MARGIN do
-                       all_black_flag = 0
-                     end
-    all_black_flag = if Enum.at(map_sens_list,1) > @black_MARGIN do
-                       all_black_flag = 0
-                     end
-    all_black_flag = if Enum.at(map_sens_list,2) > @black_MARGIN do
-                       all_black_flag = 0
-                     end
-    all_black_flag = if Enum.at(map_sens_list,3) > @black_MARGIN do
-                      all_black_flag = 0
-                     end
-    all_black_flag = if Enum.at(map_sens_list,4) > @black_MARGIN do
-                       all_black_flag = 0
-                     end
-    all_black_flag = if Enum.at(map_sens_list,5) > @black_MARGIN do
-                       all_black_flag = 0
-                     end
+    all_black_flag =
+      if Enum.at(map_sens_list, 0) > @black_MARGIN do
+        all_black_flag = 0
+      end
 
+    all_black_flag =
+      if Enum.at(map_sens_list, 1) > @black_MARGIN do
+        all_black_flag = 0
+      end
+
+    all_black_flag =
+      if Enum.at(map_sens_list, 2) > @black_MARGIN do
+        all_black_flag = 0
+      end
+
+    all_black_flag =
+      if Enum.at(map_sens_list, 3) > @black_MARGIN do
+        all_black_flag = 0
+      end
+
+    all_black_flag =
+      if Enum.at(map_sens_list, 4) > @black_MARGIN do
+        all_black_flag = 0
+      end
+
+    all_black_flag =
+      if Enum.at(map_sens_list, 5) > @black_MARGIN do
+        all_black_flag = 0
+      end
 
     # IO.inspect(all_black_flag)
-    weighted_sum_list = map_sens_list |> Enum.zip(@weights) |> Enum.map(fn {map,weight} -> map*weight end)
+    weighted_sum_list =
+      map_sens_list |> Enum.zip(@weights) |> Enum.map(fn {map, weight} -> map * weight end)
+
     weighted_sum = Enum.sum(weighted_sum_list)
     sum = Enum.sum(map_sens_list)
     # IO.inspect(weighted_sum)
     # IO.inspect(sum)
 
-    pos = if sum != 0 do
-            pos = weighted_sum / sum;
-          end
+    pos =
+      if sum != 0 do
+        pos = weighted_sum / sum
+      end
+
     # IO.inspect(pos)
 
-    error = if all_black_flag == 1 do
-              error = if prev_error > 0 do
-                        error = 2.5;
-                      else
-                        error = -2.5;
-                      end
-            else
-              error = pos;
-            end
+    error =
+      if all_black_flag == 1 do
+        error =
+          if prev_error > 0 do
+            error = 2.5
+          else
+            error = -2.5
+          end
+      else
+        error = pos
+      end
 
     # IO.inspect(error)
-    {error,prev_error}
+    {error, prev_error}
   end
 
-  def calculate_correction(error,prev_error,cumulative_error) do
-
+  def calculate_correction(error, prev_error, cumulative_error) do
     error = error * 10
     difference = error - prev_error
     cumulative_error = cumulative_error + error
 
     # cumulative_error = bound(cumulative_error, -30, 30);
-    cumulative_error = if cumulative_error < -30 do
-                         cumulative_error = -30
-                       end
-    cumulative_error = if cumulative_error > 30 do
-                         cumulative_error = 30
-                       end
+    cumulative_error =
+      if cumulative_error < -30 do
+        cumulative_error = -30
+      end
+
+    cumulative_error =
+      if cumulative_error > 30 do
+        cumulative_error = 30
+      end
 
     # correction = read_pid_const().kp * error + read_pid_const().ki * cumulative_error + read_pid_const().kd * difference;
-    correction = (200 * error) + (10 * cumulative_error) + (1 * difference)
+    correction = 1.7 * error + 0.0001 * cumulative_error + 0 * difference
     prev_error = error
-    {error,prev_error,correction}
+    {error, prev_error, correction}
   end
 
-  def my_motion(left_duty_cycle,right_duty_cycle) do
+  def my_motion(left_duty_cycle, right_duty_cycle) do
     IO.inspect(left_duty_cycle)
     IO.inspect(right_duty_cycle)
-    {_,lvalue} = Enum.at(@pwm_pins,0)
-    {_,rvalue} = Enum.at(@pwm_pins,1)
+    {_, lvalue} = Enum.at(@pwm_pins, 0)
+    {_, rvalue} = Enum.at(@pwm_pins, 1)
     Pigpiox.Pwm.gpio_pwm(lvalue, left_duty_cycle)
     Pigpiox.Pwm.gpio_pwm(rvalue, right_duty_cycle)
   end
+
   @doc """
   Tests white line sensor modules reading
 
@@ -174,12 +209,11 @@ defmodule Line_follower do
   def test_wlf_sensors do
     # Logger.debug("Testing white line sensors connected ")
     sensor_ref = Enum.map(@sensor_pins, fn {atom, pin_no} -> configure_sensor({atom, pin_no}) end)
-    sensor_ref = Enum.map(sensor_ref, fn{_atom, ref_id} -> ref_id end)
+    sensor_ref = Enum.map(sensor_ref, fn {_atom, ref_id} -> ref_id end)
     sensor_ref = Enum.zip(@ref_atoms, sensor_ref)
-    map_sens_list = get_lfa_readings([1,2,3,4,5], sensor_ref)
+    map_sens_list = get_lfa_readings([1, 2, 3, 4, 5], sensor_ref)
     map_sens_list
   end
-
 
   @doc """
   Tests IR Proximity sensor's readings
@@ -196,10 +230,12 @@ defmodule Line_follower do
   """
   def test_ir do
     Logger.debug("Testing IR Proximity Sensors")
-    ir_ref = Enum.map(@ir_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :input, pull_mode: :pullup) end)
-    ir_values = Enum.map(ir_ref,fn {_, ref_no} -> GPIO.read(ref_no) end)
-  end
 
+    ir_ref =
+      Enum.map(@ir_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :input, pull_mode: :pullup) end)
+
+    ir_values = Enum.map(ir_ref, fn {_, ref_no} -> GPIO.read(ref_no) end)
+  end
 
   @doc """
   Tests motion of the Robot
@@ -216,11 +252,10 @@ defmodule Line_follower do
     Logger.debug("Testing Motion of the Robot ")
     motor_ref = Enum.map(@motor_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
     pwm_ref = Enum.map(@pwm_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
-    Enum.map(pwm_ref,fn {_, ref_no} -> GPIO.write(ref_no, 1) end)
-    motion_list = [@forward,@stop,@backward,@stop,@left,@stop,@right,@stop]
-    Enum.each(motion_list, fn motion -> motor_action(motor_ref,motion) end)
+    Enum.map(pwm_ref, fn {_, ref_no} -> GPIO.write(ref_no, 1) end)
+    motion_list = [@forward, @stop, @backward, @stop, @left, @stop, @right, @stop]
+    Enum.each(motion_list, fn motion -> motor_action(motor_ref, motion) end)
   end
-
 
   @doc """
   Controls speed of the Robot
@@ -242,7 +277,6 @@ defmodule Line_follower do
     Enum.map(@duty_cycles, fn value -> motion_pwm(value) end)
   end
 
-
   @doc """
   Supporting function for test_wlf_sensors
   Configures sensor pins as input or output
@@ -250,7 +284,7 @@ defmodule Line_follower do
   [cs: output, clock: output, address: output, dataout: input]
   """
   defp configure_sensor({atom, pin_no}) do
-    if (atom == :dataout) do
+    if atom == :dataout do
       GPIO.open(pin_no, :input, pull_mode: :pullup)
     else
       GPIO.open(pin_no, :output)
@@ -271,29 +305,42 @@ defmodule Line_follower do
     append_sensor_list = sensor_list ++ [5]
     temp_sensor_list = [5 | append_sensor_list]
 
-    my_sens_list = append_sensor_list
-                   |> Enum.with_index
-                   |> Enum.map(fn {sens_num, sens_idx} ->
-                   analog_read(sens_num, sensor_ref, Enum.fetch(temp_sensor_list, sens_idx))
-                   end)
+    my_sens_list =
+      append_sensor_list
+      |> Enum.with_index()
+      |> Enum.map(fn {sens_num, sens_idx} ->
+        analog_read(sens_num, sensor_ref, Enum.fetch(temp_sensor_list, sens_idx))
+      end)
+
     # IO.inspect(my_sens_list)
 
-    bounded_sens_list_L = my_sens_list |> Enum.map(fn x -> if x < @black_MARGIN do
-                                                            x = @black_MARGIN
-                                                            x
-                                                           else
-                                                            x
-                                                           end
-                                                           end)
+    bounded_sens_list_L =
+      my_sens_list
+      |> Enum.map(fn x ->
+        if x < @black_MARGIN do
+          x = @black_MARGIN
+          x
+        else
+          x
+        end
+      end)
 
-   bounded_sens_list = bounded_sens_list_L |> Enum.map(fn x -> if x > @white_MARGIN do
-                                                            x = @white_MARGIN
-                                                            x
-                                                           else
-                                                            x
-                                                           end
-                                                           end)
-    map_sens_list = bounded_sens_list |> Enum.map (fn x -> round((x-@black_MARGIN)*(@white_MARGIN/(@white_MARGIN-@black_MARGIN))) end)
+    bounded_sens_list =
+      bounded_sens_list_L
+      |> Enum.map(fn x ->
+        if x > @white_MARGIN do
+          x = @white_MARGIN
+          x
+        else
+          x
+        end
+      end)
+
+    map_sens_list =
+      bounded_sens_list
+      |> Enum.map(fn x ->
+        round((x - @black_MARGIN) * (@white_MARGIN / (@white_MARGIN - @black_MARGIN)))
+      end)
 
     # IO.inspect(map_sens_list)
     Enum.each(0..5, fn n -> provide_clock(sensor_ref) end)
@@ -307,32 +354,35 @@ defmodule Line_follower do
   Supporting function for test_wlf_sensors
   """
   defp analog_read(sens_num, sensor_ref, {_, sensor_atom_num}) do
-
     GPIO.write(sensor_ref[:cs], 0)
     %{^sensor_atom_num => sensor_atom} = @lf_sensor_map
+
     Enum.reduce(0..9, @lf_sensor_data, fn n, acc ->
-                                          read_data(n, acc, sens_num, sensor_ref, sensor_atom_num)
-                                          |> clock_signal(n, sensor_ref)
-                                        end)[sensor_atom]
+      read_data(n, acc, sens_num, sensor_ref, sensor_atom_num)
+      |> clock_signal(n, sensor_ref)
+    end)[sensor_atom]
   end
 
   @doc """
   Supporting function for test_wlf_sensors
   """
   defp read_data(n, acc, sens_num, sensor_ref, sensor_atom_num) do
-    if (n < 4) do
-
-      if (((sens_num) >>> (3 - n)) &&& 0x01) == 1 do
+    if n < 4 do
+      if (sens_num >>> (3 - n) &&& 0x01) == 1 do
         GPIO.write(sensor_ref[:address], 1)
       else
         GPIO.write(sensor_ref[:address], 0)
       end
+
       Process.sleep(1)
     end
 
     %{^sensor_atom_num => sensor_atom} = @lf_sensor_map
-    if (n <= 9) do
-      Map.update!(acc, sensor_atom, fn sensor_atom -> ( sensor_atom <<< 1 ||| GPIO.read(sensor_ref[:dataout]) ) end)
+
+    if n <= 9 do
+      Map.update!(acc, sensor_atom, fn sensor_atom ->
+        sensor_atom <<< 1 ||| GPIO.read(sensor_ref[:dataout])
+      end)
     end
   end
 
@@ -356,8 +406,11 @@ defmodule Line_follower do
   @doc """
   Supporting function for test_motion
   """
-  defp motor_action(motor_ref,motion) do
-    motor_ref |> Enum.zip(motion) |> Enum.each(fn {{_, ref_no}, value} -> GPIO.write(ref_no, value) end)
+  defp motor_action(motor_ref, motion) do
+    motor_ref
+    |> Enum.zip(motion)
+    |> Enum.each(fn {{_, ref_no}, value} -> GPIO.write(ref_no, value) end)
+
     Process.sleep(2000)
   end
 
@@ -378,5 +431,4 @@ defmodule Line_follower do
   defp pwm(duty) do
     Enum.each(@pwm_pins, fn {_atom, pin_no} -> Pigpiox.Pwm.gpio_pwm(pin_no, duty) end)
   end
-
 end
