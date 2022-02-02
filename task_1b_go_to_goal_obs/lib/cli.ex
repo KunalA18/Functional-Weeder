8250fd3a5f09c6728b57acc4ae46426ac7daf0d0
Om Sheladia <84867886+omsheladia@users.noreply.github.com>
2022-02-02 23:09:02 +0530

    seeder

--- task_1b_go_to_goal_obs/lib/cli.ex
+++ task_1b_go_to_goal_obs/lib/cli.ex
@@ -1,205 +1,310 @@
-defmodule ToyRobot.CLI do
-  def main(args) do
-    args |> parse_args |> process_args
-  end
+defmodule FW_DEMO do
+  @moduledoc """
+  Documentation for `FW_DEMO`.
 
-  def parse_args(args) do
-    {params, _, _} =  OptionParser.parse(args, switches: [help: :boolean])
-    params
-  end
+  Different functions provided for testing components of Alpha Bot.
+  test_wlf_sensors  - to test white line sensors
+  test_ir           - to test IR proximity sensors
+  test_motion       - to test motion of the Robot
+  test_pwm          - to test speed of the Robot
+  test_servo_a      - to test servo motor A
+  test_servo_b      - to test servo motor B
+  """
 
-  def process_args([help: true]) do
-    print_help_message()
-  end
 
-  def process_args(_) do
-    IO.puts("Welcome to the [[ Task 1B ]] Toy Robot simulator!")
-    print_help_message()
-    receive_command()
-  end
+  require Logger
+  use Bitwise
+  alias Circuits.GPIO
 
-  @commands %{
-    "place"  => "Places the Robot into X,Y facing F (Default is 1, A, North). " <>
-                "Where facing is: north, west, south or east. " <>
-                "Format: \"place X, Y, F\".",
-    "start"  => "Define start position of robot (Default is 1, A, North). Format: \"start X, Y, F\".",
-    "stop"   => "Specify the goal position for the robot to stop at. " <>
-                "And pass a process name that listens for each action of Toy Robot.",
-    "report" => "The Toy Robot reports about its position.",
-    "move"   => "Moves the robot one position forward.",
-    "left"   => "Rotates the robot to the left.",
-    "right"  => "Rotates the robot to the right.",
-    "quit"   => "Quits the simulator."
-  }
+  @sensor_pins [cs: 5, clock: 25, address: 24, dataout: 23]
+  @ir_pins [dr: 16, dl: 19]
+  @motor_pins [lf: 12, lb: 13, rf: 20, rb: 21]
+  @pwm_pins [enl: 6, enr: 26]
+  @servo_a_pin 27
+  @servo_b_pin 22
+  @servo_c_pin 5
+
+  @ref_atoms [:cs, :clock, :address, :dataout]
+  @lf_sensor_data %{sensor0: 0, sensor1: 0, sensor2: 0, sensor3: 0, sensor4: 0, sensor5: 0}
+  @lf_sensor_map %{0 => :sensor0, 1 => :sensor1, 2 => :sensor2, 3 => :sensor3, 4 => :sensor4, 5 => :sensor5}
+
+  @forward [0, 1, 1, 0]
+  @backward [0, 1, 0, 1]
+  @left [0, 1, 1, 0]
+  @right [1, 0, 0, 1]
+  @stop [0, 0, 0, 0]
+
+  @duty_cycles [150, 70, 0]
+  @pwm_frequency 50
 
   @doc """
-  Receive commands passed to the CLI, parse and execute it.
+  Tests white line sensor modules reading
+
+  Example:
+
+      iex> FW_DEMO.test_wlf_sensors
+      [0, 958, 851, 969, 975, 943]  // on white surface
+      [0, 449, 356, 312, 321, 267]  // on black surface
   """
-  defp receive_command(robot \\ nil) do
-    IO.gets("> ")
-    |> String.trim
-    |> String.downcase
-    |> String.split(" ")
-    |> execute_command(robot)
+  def test_wlf_sensors do
+    Logger.debug("Testing white line sensors connected ")
+    sensor_ref = Enum.map(@sensor_pins, fn {atom, pin_no} -> configure_sensor({atom, pin_no}) end)
+    sensor_ref = Enum.map(sensor_ref, fn{_atom, ref_id} -> ref_id end)
+    sensor_ref = Enum.zip(@ref_atoms, sensor_ref)
+    get_lfa_readings([0,1,2,3,4], sensor_ref)
+  end
+
+
+  @doc """
+  Tests IR Proximity sensor's readings
+
+  Example:
+
+      iex> FW_DEMO.test_ir
+      [1, 1]     // No obstacle
+      [1, 0]     // Obstacle in front of Right IR Sensor
+      [0, 1]     // Obstacle in front of Left IR Sensor
+      [0, 0]     // Obstacle in front of both Sensors
+
+  Note: You can adjust the potentiometer provided on the IR sensor to get proper results
+  """
+  def test_ir do
+    Logger.debug("Testing IR Proximity Sensors")
+    ir_ref = Enum.map(@ir_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :input, pull_mode: :pullup) end)
+    ir_values = Enum.map(ir_ref,fn {_, ref_no} -> GPIO.read(ref_no) end)
+  end
+
+
+  @doc """
+  Tests motion of the Robot
+
+  Example:
+
+      iex> FW_DEMO.test_motion
+      :ok
+
+  Note: On executing above function Robot will move forward, backward, left, right
+  for 500ms each and then stops
+  """
+  def test_motion do
+    Logger.debug("Testing Motion of the Robot ")
+    motor_ref = Enum.map(@motor_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
+    pwm_ref = Enum.map(@pwm_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
+    Enum.map(pwm_ref,fn {_, ref_no} -> GPIO.write(ref_no, 1) end)
+    motion_list = [@forward,@stop,@backward,@stop,@left,@stop,@right,@stop]
+    Enum.each(motion_list, fn motion -> motor_action(motor_ref,motion) end)
+  end
+
+
+  @doc """
+  Controls speed of the Robot
+
+  Example:
+
+      iex> FW_DEMO.test_pwm
+      Forward with pwm value = 150
+      Forward with pwm value = 70
+      Forward with pwm value = 0
+      {:ok, :ok, :ok}
+
+  Note: On executing above function Robot will move in forward direction with different velocities
+  """
+  def test_pwm do
+    Logger.debug("Testing PWM for Motion control")
+    motor_ref = Enum.map(@motor_pins, fn {_atom, pin_no} -> GPIO.open(pin_no, :output) end)
+    motor_action(motor_ref, @forward)
+    Enum.map(@duty_cycles, fn value -> motion_pwm(value) end)
+  end
+
+
+  @doc """
+  Controls angle of serve motor A
+
+  Example:
+
+      iex> FW_DEMO.test_servo_a(90)
+      :ok
+
+  Note: On executing above function servo motor A will rotate by 90 degrees. You can provide
+  values from 0 to 180
+  """
+  def test_servo_a(angle) do
+    Logger.debug("Testing Servo A")         
+    val = trunc(((2.5 + 10.0 * angle / 180) / 100 ) * 255)
+    Pigpiox.Pwm.set_pwm_frequency(@servo_a_pin, @pwm_frequency)
+    Pigpiox.Pwm.gpio_pwm(@servo_a_pin, val)
+        
+  end
+
+  def arm_code do
+    
+    test_servo_b(120)
+    Process.sleep(1000)
+    test_servo_a(60)
+    Process.sleep(1000)
+    test_servo_b(60)
+    Process.sleep(1000)
+    test_servo_a(0)
+    Process.sleep(1250)
+    test_servo_b(120)
+    Process.sleep(1500)
+    test_servo_a(60)
+    Process.sleep(1000)
+    
+
+
+  end
+
+  def seeder do
+    
+    test_servo_c(60)
+    Process.sleep(2500)
+    test_servo_c(120)
+    Process.sleep(2500)
+    test_servo_c(180)
+    Process.sleep(2500)
+
+    
+
+
+  end
+  def test_servo_c(angle) do
+    Logger.debug("Testing Servo C")
+    val = trunc(((2.5 + 10.0 * angle / 180) / 100 ) * 255)
+    Pigpiox.Pwm.set_pwm_frequency(@servo_c_pin, @pwm_frequency)
+    Pigpiox.Pwm.gpio_pwm(@servo_c_pin, val)
   end
 
   @doc """
-  Execute the 'place' commmand when called with no parameters.
-  Place the robot at default location of (1, A, NORTH).
+  Controls angle of serve motor B
+
+  Example:
+
+      iex> FW_DEMO.test_servo_b(90)
+      :ok
+
+  Note: On executing above function servo motor B will rotate by 90 degrees. You can provide
+  values from 0 to 180
   """
-  defp execute_command(["place"], _robot) do
-    {:ok, robot} = ToyRobot.place
-    robot |> receive_command
+  def test_servo_b(angle) do
+    Logger.debug("Testing Servo B")
+    val = trunc(((2.5 + 10.0 * angle / 180) / 100 ) * 255)
+    Pigpiox.Pwm.set_pwm_frequency(@servo_b_pin, @pwm_frequency)
+    Pigpiox.Pwm.gpio_pwm(@servo_b_pin, val)
   end
 
   @doc """
-  Execute the 'place' commmand when called with parameters
-  passed as: "\"place X, Y, F\"."
-  Place the robot at given location of (X, Y, F).
-  """
-  defp execute_command(["place" | params], _robot) do
-    {x, y, facing} = process_place_params(params)
+  Supporting function for test_wlf_sensors
+  Configures sensor pins as input or output
 
-    case ToyRobot.place(x, y, facing) do
-      {:ok, robot} ->
-        receive_command(robot)
-      {:failure, message} ->
-        IO.puts message
-        receive_command()
+  [cs: output, clock: output, address: output, dataout: input]
+  """
+  defp configure_sensor({atom, pin_no}) do
+    if (atom == :dataout) do
+      GPIO.open(pin_no, :input, pull_mode: :pullup)
+    else
+      GPIO.open(pin_no, :output)
     end
   end
 
   @doc """
-  Execute the 'start' commmand when called with no parameters.
-  Provide START position to the robot as default location of (1, A, NORTH).
+  Supporting function for test_wlf_sensors
+  Reads the sensor values into an array. "sensor_list" is used to provide list
+  of the sesnors for which readings are needed
+
+
+  The values returned are a measure of the reflectance in abstract units,
+  with higher values corresponding to lower reflectance (e.g. a black
+  surface or void)
   """
-  defp execute_command(["start"], _robot) do
-    {:ok, robot} = ToyRobot.start(1, :a, :north)
-    robot |> receive_command
+  defp get_lfa_readings(sensor_list, sensor_ref) do
+    append_sensor_list = sensor_list ++ [5]
+    temp_sensor_list = [5 | append_sensor_list]
+    IO.inspect(append_sensor_list
+        |> Enum.with_index
+        |> Enum.map(fn {sens_num, sens_idx} ->
+              analog_read(sens_num, sensor_ref, Enum.fetch(temp_sensor_list, sens_idx))
+              end))
+    Enum.each(0..5, fn n -> provide_clock(sensor_ref) end)
+    GPIO.write(sensor_ref[:cs], 1)
+    Process.sleep(250)
+    get_lfa_readings(sensor_list, sensor_ref)
   end
 
   @doc """
-  Execute the 'start' commmand when called with parameters
-  passed as: "\"start X, Y, F\"."
-  Provide START position to the robot as given location of (X, Y, F).
+  Supporting function for test_wlf_sensors
   """
-  defp execute_command(["start" | params], _robot) do
-    {x, y, facing} = process_place_params(params)
+  defp analog_read(sens_num, sensor_ref, {_, sensor_atom_num}) do
 
-    case ToyRobot.start(x, y, facing) do
-      {:ok, robot} ->
-        receive_command(robot)
-      {:failure, message} ->
-        IO.puts message
-        receive_command()
+    GPIO.write(sensor_ref[:cs], 0)
+    %{^sensor_atom_num => sensor_atom} = @lf_sensor_map
+    Enum.reduce(0..9, @lf_sensor_data, fn n, acc ->
+                                          read_data(n, acc, sens_num, sensor_ref, sensor_atom_num)
+                                          |> clock_signal(n, sensor_ref)
+                                        end)[sensor_atom]
+  end
+
+  @doc """
+  Supporting function for test_wlf_sensors
+  """
+  defp read_data(n, acc, sens_num, sensor_ref, sensor_atom_num) do
+    if (n < 4) do
+
+      if (((sens_num) >>> (3 - n)) &&& 0x01) == 1 do
+        GPIO.write(sensor_ref[:address], 1)
+      else
+        GPIO.write(sensor_ref[:address], 0)
+      end
+      Process.sleep(1)
+    end
+
+    %{^sensor_atom_num => sensor_atom} = @lf_sensor_map
+    if (n <= 9) do
+      Map.update!(acc, sensor_atom, fn sensor_atom -> ( sensor_atom <<< 1 ||| GPIO.read(sensor_ref[:dataout]) ) end)
     end
   end
 
   @doc """
-  Execute the 'stop' commmand when called with parameters
-  passed as: "\"stop X, Y\"."
-  Provide STOP position to the robot as given location of (X, Y).
+  Supporting function for test_wlf_sensors used for providing clock pulses
   """
-  defp execute_command(["stop" | params], robot) do
-    {goal_x, goal_y} = process_end_params(params)
-
-    # file object to read obstacle locations from given .txt file
-    obs_data = File.read!("obstacles.txt")
-
-    # parse the .txt file data to get list of obstacle locations
-    list_obs = obs_data |> String.trim |> String.replace(" | ", "\n") |> String.split("\n")
-    list_obs = Enum.map(list_obs, fn params -> String.split(params, " ") |> process_place_params() end)
-    # IO.inspect(list_obs)
-
-    # file object to write each action taken by Toy Robot
-    {:ok, out_file} = File.open("task_1b_output.txt", [:write])
-
-    # spawn and register CLI server process
-    # with 'listen_from_client' callback function
-    pid = spawn_link(fn -> listen_from_client([], list_obs, out_file) end)
-    Process.register(pid, :cli_robot_state)
-
-    # invoke the Toy Robot's stop function, provide necessary parameters
-    case robot |> ToyRobot.stop(goal_x, goal_y, :cli_robot_state) do
-      {:ok, robot} ->
-        receive_command(robot)
-      {:failure, message} ->
-        IO.puts message
-        receive_command()
-      _ -> receive_command()
-    end
+  defp provide_clock(sensor_ref) do
+    GPIO.write(sensor_ref[:clock], 1)
+    GPIO.write(sensor_ref[:clock], 0)
   end
 
   @doc """
-  Listen to 'toy_robot.ex' client and receives Toy Robot's current status
-  after every action taken by the Toy Robot.
-  Indicate the presence of obstacle to the ToyRobot client process,
-  if located ahead of robot's current position and facing
-  Write the action taken with position of robot to a text (.txt) file.
+  Supporting function for test_wlf_sensors used for providing clock pulses
   """
-  def listen_from_client(state, list_obs, out_file) do
-    receive do
-      {:toyrobot_status, robot_x, robot_y, robot_face} ->
-        IO.puts("Received by CLI Server: #{robot_x}, #{robot_y}, #{robot_face}")
-        IO.binwrite(out_file, "#{robot_x}, #{robot_y}, #{robot_face}\n")
-        #line added by me
-        IO.inspect(Process.whereis(:client_toyrobot), label: "Where is")
-        #send(:client_toyrobot, {:obstacle_presence, Enum.member?(list_obs, {robot_x, robot_y, robot_face})})
-        #-------------
-        send(:client_toyrobot, {:obstacle_presence, Enum.member?(list_obs, {robot_x, robot_y, robot_face})})
-        listen_from_client([{robot_x, robot_y, robot_face} | state], list_obs, out_file)
-    end
+  defp clock_signal(acc, n, sensor_ref) do
+    GPIO.write(sensor_ref[:clock], 1)
+    GPIO.write(sensor_ref[:clock], 0)
+    acc
   end
 
-  defp execute_command(["left"], robot) do
-    robot |> ToyRobot.left |> receive_command
+  @doc """
+  Supporting function for test_motion
+  """
+  defp motor_action(motor_ref,motion) do
+    motor_ref |> Enum.zip(motion) |> Enum.each(fn {{_, ref_no}, value} -> GPIO.write(ref_no, value) end)
+    Process.sleep(500)
   end
 
-  defp execute_command(["right"], robot) do
-    robot |> ToyRobot.right |> receive_command
+  @doc """
+  Supporting function for test_pwm
+  """
+  defp motion_pwm(value) do
+    IO.puts("Forward with pwm value = #{value}")
+    pwm(value)
+    Process.sleep(2000)
   end
 
-  defp execute_command(["move"], robot) do
-    robot |> ToyRobot.move |> receive_command
+  @doc """
+  Supporting function for test_pwm
+
+  Note: "duty" variable can take value from 0 to 255. Value 255 indicates 100% duty cycle
+  """
+  defp pwm(duty) do
+    Enum.each(@pwm_pins, fn {_atom, pin_no} -> Pigpiox.Pwm.gpio_pwm(pin_no, duty) end)
   end
 
-  defp execute_command(["report"], nil) do
-    IO.puts "The robot has not been placed yet."
-    receive_command()
-  end
-
-  defp execute_command(["report"], robot) do
-    {x, y, facing} = robot |> ToyRobot.report
-    IO.puts String.upcase("#{x},#{y},#{facing}")
-
-    receive_command(robot)
-  end
-
-  defp execute_command(["quit"], _robot) do
-    IO.puts "\nConnection lost"
-  end
-
-  defp execute_command(_unknown, robot) do
-    IO.puts("\nInvalid command. I don't know what to do.")
-    print_help_message()
-
-    receive_command(robot)
-  end
-
-  defp process_place_params(params) do
-    [x, y, facing] = params |> Enum.join("") |> String.split(",") |> Enum.map(&String.trim/1)
-    {String.to_integer(x), String.to_atom(y), String.to_atom(facing)}
-  end
-
-  defp process_end_params(params) do
-    [x, y] = params |> Enum.join("") |> String.split(",") |> Enum.map(&String.trim/1)
-    {String.to_integer(x), String.to_atom(y)}
-  end
-
-  defp print_help_message do
-    IO.puts("\nThe simulator supports following commands:\n")
-    @commands
-    |> Enum.map(fn({command, description}) -> IO.puts("  #{command} - #{description}") end)
-    IO.puts("")
-  end
 end
