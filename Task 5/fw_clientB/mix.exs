26cb14ea5ccb3f968cc28b8cefa79d2fe32f9974
toshan-luktuke <toshan1603@gmail.com>
2022-02-11 11:44:56 +0530

    Add FWClientRobotB files

--- Task 5/fw_clientB/mix.exs
+++ Task 5/fw_clientB/mix.exs
@@ -1,14 +1,14 @@
-defmodule FWClientRobotA.MixProject do
+defmodule FWClientRobotB.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :task_4c_client_robota,
+      app: :task_4c_client_robotb,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: FWClientRobotA]
+      escript: [main_module: FWClientRobotB]
     ]
   end
 
@@ -32,6 +32,4 @@ defmodule FWClientRobotA.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
-
-
 end
